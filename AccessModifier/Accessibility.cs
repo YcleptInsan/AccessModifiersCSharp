a70dc426e0d5e108bdf0d3f7c7983dac26ce05ff
ycleptinsan <nickmwise@gmail.com>
2020-07-26 16:58:23 -0700

    initial commit. Working  with C# access modifiers and learning their usecases.

---
+++ AccessModifier/Accessibility.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace AccessModifier
+{
+    class Accessibility 
+    {
+        static void Main()
+        {
+            Public accessible = new Public();
+            accessible.Accessible();
+           // The  code  below will give  an  error due  to  its  protection level
+           // accessible.PrivateAccessiblity();
+
+           // The Code  below is  inacccessible due to it not being in derrived class  or  apart  of the  same class
+           // Protected newProtected  = new Protected();
+           // newProtected.ProtectedAccessibility();
+            Access newAccess =  new Access();
+            newAccess.Protect();
+        }
+    }
+
+    public class Public : Private
+    {
+        public void Accessible()
+        {
+            Console.WriteLine("Public access levels allow for this code to be acccessed in the same assembly or another assemby that references it.");
+        }
+    }
+
+    public class Private 
+    {
+        private void PrivateAccessiblity()
+        {
+            Console.WriteLine("Private members can be accessed only by code in the same class or struct.");
+        }
+    }
+
+    public class Protected
+    {
+        protected void ProtectedAccessibility()
+        {
+         
+            Console.WriteLine("The type or member can be accessed only by code in the same class, or in a class that is derived from that class");
+        }
+
+    }
+
+    public class Access
+    {
+       // private field:
+       private int priv = 3;
+
+        // protected internal property:
+       protected internal int protec
+       {
+           get { return this.priv; }
+       }
+
+       // protected method:
+       protected internal void Protect() 
+       { 
+           Console.WriteLine(protec);
+       }
+
+    }
+}
